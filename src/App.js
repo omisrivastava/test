38556ff8dd8c56d27c1d842f0d770301d72769e8
omisrivastava <srivastaomi@gmail.com>
2023-01-14 16:29:12 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="counterContainer">
+      <h2 className="heading">
+        Omi's Counter App
+      </h2>
+      <div className="countValue">Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(-count)}>Change Sign</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
